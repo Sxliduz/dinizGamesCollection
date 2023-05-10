6f64f696023e0f4433097ff651df0b30b0c2d99f
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-05-10 14:38:11 -0300

    Project created

---
+++ src/main/java/com/diniz/games/GamesCollectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.diniz.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesCollectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesCollectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diniz/games/GamesCollectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diniz.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesCollectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
